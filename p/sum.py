cf903ca069899778f825c565088a46598c303484
xiangsisi-1 <1569342580@qq.com>
2019-09-25 18:35:25 +0800

    new python

---
+++ p/sum.py
@@ -0,0 +1,24 @@
+'''sum = 0
+for x in range(101):
+  sum= sum + x
+print (sum)     #1到100的和 '''
+
+'''sum=0
+n=99
+while n > 0:
+  sum = sum + n
+  n = n- 2
+print(sum) #100以内所有奇数之和'''
+
+'''n=1
+while n<= 100 :
+  print(n)
+  n= n + 1
+print('end') #打印1-100数值'''
+
+n=1
+while n <=10:
+  n= n+1
+  if n % 2 = 0:
+    continue
+  print(n)
